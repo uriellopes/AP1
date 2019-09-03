603ae8e47a9e070dce1cda3baf536f5c9dc263a9
uriellopes <urielrlps@gmail.com>
2019-09-03 18:04:13 -0300

    Criado arquivo tempo.cpp com as funções da classe Tempo

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//g++ -Wall -pedantic -g -O0 -o exec main.cpp concessionaria.cpp
+//g++ -Wall -pedantic -g -O0 -o exec main.cpp concessionaria.cpp tempo.cpp
 #include <iostream>
 #include <vector>
 #include "concessionaria.h"
@@ -6,20 +6,31 @@
 using namespace std;
 
 int main() {
-    
-    vector<Concessionaria> concessionarias;
 
-    concessionarias.push_back(Concessionaria("Fiat", 12121));
-    concessionarias.push_back(Concessionaria("Gol", 2323));
-    concessionarias.push_back(Concessionaria("Punto", 1223121));
+    int dia, mes, ano;
 
-    for(vector<Concessionaria>::iterator i = concessionarias.begin(); i != concessionarias.end(); i++ ) {
-        cout << *i << endl;
-    }
+    cin >> dia;
+    cin >> mes;
+    cin >> ano;
 
-    for(unsigned int i = 0; i < concessionarias.size(); i++ ) {
-        cout << concessionarias[i] << endl;
-    }
+    Tempo data = Tempo(dia,mes,ano);
+
+    cout << data << endl;
+
+
+    // vector<Concessionaria> concessionarias;
+
+    // concessionarias.push_back(Concessionaria("Fiat", 12121));
+    // concessionarias.push_back(Concessionaria("Gol", 2323));
+    // concessionarias.push_back(Concessionaria("Punto", 1223121));
+
+    // for(vector<Concessionaria>::iterator i = concessionarias.begin(); i != concessionarias.end(); i++ ) {
+    //     cout << *i << endl;
+    // }
+
+    // for(unsigned int i = 0; i < concessionarias.size(); i++ ) {
+    //     cout << concessionarias[i] << endl;
+    // }
 
     return 0;
 }
