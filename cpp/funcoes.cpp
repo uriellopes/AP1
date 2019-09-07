db48daa1d1426c4b190da671a68cb189d0e8b298
uriellopes <urielrlps@gmail.com>
2019-09-07 13:35:09 -0300

    Nova função para verificar se o input de data do usuario esta correto

--- cpp/funcoes.cpp
+++ cpp/funcoes.cpp
@@ -33,6 +33,8 @@ bool checarDigito(std::string &input) {
 //Função para chegar se o input é um float
 bool checarFloat(std::string &input) {
     int ponto = 0;
+
+    //Verificar se os elementos são digitos ou '.' para casa decimais
     for (unsigned int i = 0; i < input.size(); i++) {
         if (!isdigit(input[i])) {
             if (input[i] != '.') {
@@ -43,6 +45,7 @@ bool checarFloat(std::string &input) {
         }
     }
 
+    //Se a quantidade de pontos for maior que 1 o input é invalido
     if (ponto < 2) {
         return true;
     } else {
@@ -55,9 +58,50 @@ bool checkBissexto(int &ano) {
     return (ano % 4 == 0) && (ano % 100 != 0 || ano % 400 == 0);
 }
 
+//Função para testar se o input de data do usuário é válido
+bool testeData(std::string &data) {
+    int barra = 0;
+
+    //Verificar se os elementos são digitos ou '/' para separar o dia/mes/ano
+    for (unsigned int i = 0; i < data.size(); i++) {
+        if (!isdigit(data[i])) {
+            if (data[i] != '/') {
+                return false;
+            } else {
+                //Caso a barra esteja ao lado de outra barra, no inicio ou no fim da string o input é inválido
+                if( (data[i - 1] == '/' || i == 0 || i + 1 == data.size() ) ) { 
+                    return false;
+                } else {
+                    barra++;
+                }
+            }
+        }
+    }
+
+    //Caso tenham menos ou mais de 2 barras o input é inválido
+    if (barra == 2) {
+        return true;
+    } else {
+        return false;
+    }
+
+    return true;
+}
+
+//Função que remove as barras e troca por espaços
+void converterData(std::string &data) {
+    for (unsigned int i = 0; i < data.size(); i++) {
+        if( data[i] == '/') {
+            data[i] = ' ';
+        }
+    }
+}
+
 //Função para validar se a data inserida é válida
 bool validarData(int d, int m, int a) {
     int meses[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    //Se o ano for bissexto mudar a quantidade de dias de 28 para 29 no mes de fevereiro
     if (checkBissexto(a)) {
         meses[1] = 29;
     }
@@ -75,18 +119,19 @@ void addInfo(std::vector<Concessionaria> &concessionarias) {
     concessionarias.push_back(Concessionaria("Concessionaria 2", 479378124000401));
     concessionarias.push_back(Concessionaria("Concessionaria 3", 122312123444407));
 
-    //Dados iniciais para as concessionárias
+    //Dados iniciais de carros para as concessionárias
+    //Concessionaria 1
     concessionarias[0].adicionarCarro(Automovel("Toyota", 20000.00, "AAAAAAAAAAA", Tempo(2, 1, 2019)));
     concessionarias[0].adicionarCarro(Automovel("Volkswagen", 15000.00, "BBBBBBBBBB", Tempo(17, 3, 2018)));
     concessionarias[0].adicionarCarro(Automovel("Ford", 18000.50, "CCCCCCCCCC", Tempo(31, 3, 2018)));
     concessionarias[0].adicionarCarro(Automovel("Nissan", 50500.00, "DDDDDDDDDD", Tempo(28, 8, 2019)));
     concessionarias[0].adicionarCarro(Automovel("Hyundai", 78800.00, "EEEEEEEEEE", Tempo(14, 7, 2019)));
-
+    //Concessionaria 2
     concessionarias[1].adicionarCarro(Automovel("Chevrolet", 16500.00, "FFFFFFFFFF", Tempo(1, 9, 2019)));
     concessionarias[1].adicionarCarro(Automovel("Kia", 32000.00, "GGGGGGGGGG", Tempo(17, 8, 2018)));
     concessionarias[1].adicionarCarro(Automovel("Mercedes-Benz", 45900.50, "HHHHHHHHHH", Tempo(31, 7, 2019)));
     concessionarias[1].adicionarCarro(Automovel("Renault", 69999.90, "IIIIIIIIII", Tempo(30, 6, 2019)));
-
+    //Concessionaria 3
     concessionarias[2].adicionarCarro(Automovel("Audi", 3600.00, "JJJJJJJJJJ", Tempo(29, 8, 2019)));
     concessionarias[2].adicionarCarro(Automovel("Peugeot", 7000.00, "KKKKKKKKKK", Tempo(17, 3, 2018)));
     concessionarias[2].adicionarCarro(Automovel("BMW", 125400.50, "LLLLLLLLLL", Tempo(31, 3, 2017)));
@@ -109,6 +154,7 @@ void novaConcessionaria(std::vector<Concessionaria> &concessionarias) {
     std::cin.ignore();
     std::getline(std::cin, nome);
 
+    //Loop para receber um valor de CNPJ correto
     do {
         inputValido = true;
         std::cout << "Digite o CNPJ da nova concessionaria ( Digite apenas os numeros ): ";
@@ -124,6 +170,7 @@ void novaConcessionaria(std::vector<Concessionaria> &concessionarias) {
 
     cnpj = stoll(input, nullptr, 10);
 
+    //Verificar se já existe alguma concessionária com nome ou cpnj digitado pelo usuário cadastrada
     for (unsigned int i = 0; i < concessionarias.size(); i++) {
         if (concessionarias[i].getCNPJ() == cnpj || nome.compare(concessionarias[i].getNome()) == 0) {
             existe = true;
@@ -147,59 +194,44 @@ void novaConcessionaria(std::vector<Concessionaria> &concessionarias) {
 }
 
 //Funcao que pega as informacoes do carro caso o carro nao esteja cadastrado ou a concessionaria esteja vazia
-void cadastrarCarro(Concessionaria &c, std::string chassi) {
-    std::string marca, preco, dia, mes, ano;
-    bool error = false, dataErro = false, dataValida = false;
+void cadastrarCarro(Concessionaria &c, std::string &chassi) {
+    std::string marca, preco, data;
+    bool error = false, dataValida = true;
+    int dia, mes, ano;
 
     std::cout << "Digite a marca do carro: ";
     std::cin >> marca;
 
+    //Loop para pegar um input de preço no formato correto
     do {
         if (error) {
-            std::cout << std::endl << "Digite apenas numeros float para um valor valido!!" << std::endl;
+            std::cout << std::endl << "Digite apenas numeros com um '.' separando os centavos !!" << std::endl;
         }
-        std::cout << "Digite o preco do carro (Digite apenas os numeros e com apenas um '.' para separar o decimal): ";
+        std::cout << "Digite o preco do carro (xxxxxx.xx): ";
         std::cin >> preco;
         error = !checarFloat(preco);
     } while (error);
 
+    //Loop para pegar um valor de data no formato correto e válida
     do {
-        if (dataValida) {
+        if (!dataValida) {
             std::cout << std::endl << "Digite um valor de data valido!!" << std::endl;
         }
 
-        do {
-            if (dataErro) {
-                std::cout << std::endl << "Digite um valor de dia valido!!" << std::endl;
-            }
-            std::cout << "Digite o dia da data de fabricacao do carro: ";
-            std::cin >> dia;
-            dataErro = !checarDigito(dia);
-        } while (dataErro);
+        std::cout << "Digite a data de fabricacao do carro (dd/mm/aaaa): ";
+        std::cin >> data;
 
-        do {
-            if (dataErro) {
-                std::cout << std::endl << "Digite um valor de mes valido!!" << std::endl;
-            }
-            std::cout << "Digite o mes da data de fabricacao do carro: ";
-            std::cin >> mes;
-            dataErro = !checarDigito(mes);
-        } while (dataErro);
+        if( testeData(data) ) {
+            converterData(data);
+            std::istringstream(data) >> dia >> mes >> ano;        
+            dataValida = validarData(dia, mes, ano);
+        } else {
+            dataValida = false;
+        }
 
-        do {
-            if (dataErro) {
-                std::cout << std::endl << "Digite um valor de ano valido!!" << std::endl;
-            }
-            std::cout << "Digite o ano da data de fabricacao do carro: ";
-            std::cin >> ano;
-            dataErro = !checarDigito(ano);
-        } while (dataErro);
+    } while (!dataValida);
 
-        dataValida = !validarData(stoi(dia), stoi(mes), stoi(ano));
-
-    } while (dataValida);
-
-    c.adicionarCarro(Automovel(marca, std::stof(preco), chassi, Tempo(stoi(dia), stoi(mes), stoi(ano))));
+    c.adicionarCarro(Automovel(marca, std::stof(preco), chassi, Tempo(dia, mes, ano)));
     std::cout << "Carro cadastrado com sucesso!!!" << std::endl;
 }
 
@@ -216,6 +248,7 @@ void adicionarNovoCarro(Concessionaria &c) {
     std::cout << "Digite o chassi do carro: ";
     std::cin >> chassi;
 
+    //Verificar se já existe algum carro com o chassi fornecido pelo usuário
     if (c.checkSize() > 0) {
         if (c.verificarExiste(chassi)) {
             std::cout << "Já existe um carro cadastrado com esse chassi!!" << std::endl;
@@ -241,16 +274,17 @@ void menuAumentarPreco(Concessionaria &c) {
     std::cout << "##   3 - Aumentar preco de todos os carros em %   ##" << std::endl;
     std::cout << "####################################################" << std::endl << std::endl;
 
+    //Loop para pegar um valor no formato correto
     do {
         inputValido = true;
-        std::cout << "Digite o valor da % desejado ( Digite apenas os numeros e com apenas um '.' para separar o decimal ): ";
+        std::cout << "Digite o valor da % desejado ( xx.x ): ";
         std::cin >> input;
 
         inputValido = checarFloat(input);
 
         if (!inputValido) {
             std::cout << std::endl;
-            std::cout << "Digite apenas numeros float para um valor valido!!" << std::endl;
+            std::cout << "Digite apenas os numeros e com apenas um '.' para separar o decimal!!" << std::endl;
         }
     } while (!inputValido);
 
@@ -269,6 +303,8 @@ void selecionarConcessionaria(Concessionaria &c) {
     int escolha;
     bool sair = false;
     bool error = false;
+
+    //Loop para verificar se o input é uma opção válida e caso seja, chamar a função referente a opção escolhida
     do {
         clear();
         std::cout << std::endl << "###################################" << std::endl;
@@ -291,7 +327,7 @@ void selecionarConcessionaria(Concessionaria &c) {
         }
         std::cout << "Opcao: ";
         std::cin >> input;
-
+        
         if (checarDigito(input)) {
             escolha = std::stoi(input, nullptr);
 
@@ -332,6 +368,7 @@ void showMenu(std::vector<Concessionaria> &concessionarias) {
     bool sair = false;
     bool error = false;
 
+    //Loop para verificar se o input é uma opção válida e caso seja, realizer a operação referente a escolha
     do {
         clear();
         std::cout << std::endl << "############################################################" << std::endl;
